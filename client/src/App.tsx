0bd3cbef84606cd9d76e8d885490df12cf10a618
rohiniatal-oss <rohiniatal@gmail.com>
2026-06-05 16:21:36 +0100

    Remove visible Strategy Builder panel from app shell

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import { StrategyBuilderPanel } from "@/components/StrategyBuilderPanel";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
@@ -151,7 +150,6 @@ function App() {
           <Toaster />
           <Router hook={useHashLocation}>
             <AppRouter />
-            <StrategyBuilderPanel />
             <RestartFromHereButton />
           </Router>
         </TooltipProvider>

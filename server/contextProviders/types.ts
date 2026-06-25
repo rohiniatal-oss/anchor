02ed225d4d6591d2cd3e06acd7748f1ed05e4744
rohiniatal-oss <rohiniatal@gmail.com>
2026-06-25 17:35:11 +0100

    chore: keep external research intents exhaustive

--- server/contextProviders/types.ts
+++ server/contextProviders/types.ts
@@ -58,7 +58,6 @@ export interface TaskContextProvider {
 }
 
 export type ExternalResearchIntent =
-  | "entity_research"
   | "company_research"
   | "deadline_verification"
   | "eligibility_check"
